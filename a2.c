49b1521702d752540543cb407288c76591ef5b23
JaydenYip3 <jy1937jy@gmail.com>
2025-03-11 16:39:32 -0400

    Modularized for systemWide for all active PIDs

--- a2.c
+++ a2.c
@@ -171,6 +171,47 @@ void system_Wide_output_single_PID(Flags* f,DIR* fd_path, struct dirent *fd_entr
             }
         }
 }
+void system_Wide_output_multiple_PID(DIR* fd_path, DIR* proc_dir, struct dirent *fd_entry, struct dirent *entry){
+    int count = 0;
+    while ((entry = readdir(proc_dir))){
+        char PID[20];
+        snprintf(PID, sizeof(PID), "%.9s", entry->d_name);
+
+        if (!isdigit(PID[0])){
+            continue;
+        }
+
+        if (owns_file(PID)){
+            char file_path[256];
+            snprintf(file_path, sizeof(file_path), "/proc/%s/fd", PID);
+            fd_path = opendir(file_path);
+
+            if (!fd_path){
+                //error accessing the path as a safety check
+                continue;
+            }
+            while ((fd_entry = readdir(fd_path)) != NULL){
+                if (strcmp(fd_entry->d_name, ".") == 0 || strcmp(fd_entry->d_name, "..") == 0) {
+                    continue;
+                }
+                char fd_filename[1024];
+                char fd_entry_link[512]; //this might be bad...
+                snprintf(fd_entry_link, sizeof(fd_entry_link), "/proc/%.9s/fd/%s", PID, fd_entry->d_name);
+                ssize_t length = readlink(fd_entry_link, fd_filename, sizeof(fd_filename) - 1);
+                if (length == -1){
+                    printf("%.7s %s\n",PID, fd_entry->d_name);
+                }
+                else{
+                    fd_filename[length] = '\0';
+                    printf("%-8d %-7.7s %-7.7s %s\n", count++, PID, fd_entry->d_name, fd_filename);
+                }
+            }
+            closedir(fd_path);
+        }
+
+
+    }
+}
 void table_output(struct Flags* f){
     //printf("%s", f->PID);
     struct dirent *entry;
@@ -214,45 +255,7 @@ void table_output(struct Flags* f){
             system_Wide_output_single_PID(f, fd_path,fd_entry);
         }
         else{
-            int count = 0;
-            while ((entry = readdir(proc_dir))){
-                char PID[20];
-                snprintf(PID, sizeof(PID), "%.9s", entry->d_name);
-
-                if (!isdigit(PID[0])){
-                    continue;
-                }
-
-                if (owns_file(PID)){
-                    char file_path[256];
-                    snprintf(file_path, sizeof(file_path), "/proc/%s/fd", PID);
-                    fd_path = opendir(file_path);
-
-                    if (!fd_path){
-                        //error accessing the path as a safety check
-                        continue;
-                    }
-                    while ((fd_entry = readdir(fd_path)) != NULL){
-                        if (strcmp(fd_entry->d_name, ".") == 0 || strcmp(fd_entry->d_name, "..") == 0) {
-                           continue;
-                        }
-                        char fd_filename[1024];
-                        char fd_entry_link[512]; //this might be bad...
-                        snprintf(fd_entry_link, sizeof(fd_entry_link), "/proc/%.9s/fd/%s", PID, fd_entry->d_name);
-                        ssize_t length = readlink(fd_entry_link, fd_filename, sizeof(fd_filename) - 1);
-                        if (length == -1){
-                            printf("%.7s %s\n",PID, fd_entry->d_name);
-                        }
-                        else{
-                            fd_filename[length] = '\0';
-                            printf("%-8d %-7.7s %-7.7s %s\n", count++, PID, fd_entry->d_name, fd_filename);
-                        }
-                    }
-                    closedir(fd_path);
-                }
-
-
-            }
+            system_Wide_output_multiple_PID(fd_path,proc_dir,fd_entry,entry);
         }
         printf("        ========================================\n\n");
     }
